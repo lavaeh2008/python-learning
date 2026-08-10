cc0dcc7dbb24379995650eed5eec0b801b5b008b
lavaeh2008 <lavaehheidelberg@gmail.com>
2026-08-10 18:42:30 -0400

    Create 01_print_variables.py

---
+++ 01_basics/01_print_variables.py
@@ -0,0 +1,20 @@
+# Print, Variables, and Data Types
+
+print("Hello World")
+
+name = "LaVaeh Heidelberg"
+age = 17
+major = "Computer Engineering"
+graduation_year = 2030
+
+print("My name is", name + ".")
+print("I am", age, "years old.")
+
+print("===========================")
+print("********* DIGITAL ID ********")
+print("===========================")
+print("Name:", name)
+print("Age:", age)
+print("Major:", major)
+print("Class:", graduation_year)
+print("===========================")
