b3ccbe38dffa46498bec6b42c20611cb9c4ecfc3
lavaeh2008 <lavaehheidelberg@gmail.com>
2026-08-10 18:47:36 -0400

    Create 04_simple_calculator.py

---
+++ 01_basics/04_simple_calculator.py
@@ -0,0 +1,12 @@
+# Simple Calculator
+
+print("Provide any two real numbers, and I will perform basic calculations.")
+
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
+
+print("Addition:", first_number + second_number)
+print("Subtraction:", first_number - second_number)
+print("Multiplication:", first_number * second_number)
+print("Division:", first_number / second_number)
+print("Remainder:", first_number % second_number)
