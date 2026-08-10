f0f60ae5693597565ad2c0937f01680d0f006645
lavaeh2008 <lavaehheidelberg@gmail.com>
2026-08-10 18:45:58 -0400

    Create 02_input_and_type_conversion.py

---
+++ 01_basics/02_input_and_type_conversion.py
@@ -0,0 +1,14 @@
+# Input and Type Conversion
+
+user_name = input("Hi! What's your name? ")
+major = input("What's your major? ")
+graduation_year = int(input("What year do you graduate? "))
+favorite_language = input("What's your favorite programming language? ")
+
+print()
+print("Hello", user_name + "!")
+print("Major:", major)
+print("Graduation Year:", graduation_year)
+print("Favorite Language:", favorite_language)
+print()
+print("Welcome to your coding journey!")
