892250ef78c425f4304f3af30d5f9eb7210726c0
lavaeh2008 <lavaehheidelberg@gmail.com>
2026-08-10 19:38:15 -0400

    Create 02_multiply.py

---
+++ 04_functions/02_multiply.py
@@ -0,0 +1,10 @@
+# Function With Parameters and Return Value
+
+def multiply(a, b):
+    return a * b
+
+
+first_number = int(input("Enter a number: "))
+second_number = int(input("Enter another number: "))
+
+print("The product is", multiply(first_number, second_number))
